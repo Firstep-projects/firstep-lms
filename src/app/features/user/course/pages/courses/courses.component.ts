4f1d00e49f51766d92ffae39b8433da03fc68266
Jahongir <jahongirakbarov007@gmail.com>
2025-07-19 18:09:43 +0500

    fix problems

--- src/app/features/user/course/pages/courses/courses.component.ts
+++ src/app/features/user/course/pages/courses/courses.component.ts
@@ -1,9 +1,10 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
-  imports: [NgFor],
+  imports: [NgFor, RouterLink],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
