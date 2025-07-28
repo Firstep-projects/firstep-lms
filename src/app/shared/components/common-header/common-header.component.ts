2a94a941826bb064f03e498e55816d3c05586641
Jahongir <jahongirakbarov007@gmail.com>
2025-07-28 09:30:38 +0500

    add css variables

--- src/app/shared/components/common-header/common-header.component.ts
+++ src/app/shared/components/common-header/common-header.component.ts
@@ -1,11 +1,15 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-common-header',
-    imports: [],
+    imports: [RouterLink],
     templateUrl: './common-header.component.html',
     styleUrl: './common-header.component.css',
 })
 export class CommonHeaderComponent {
     @Input() label: string = '';
+    @Input() back: boolean = false;
+    @Input() backLink: string = "/courses";
+    @Input() styleClass: string = "";
 }
